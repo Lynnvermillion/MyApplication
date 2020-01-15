a8c574e1ea78340d28a9e393d625b44bed2aba09
utpallama <uslama@hotmail.co.uk>
2020-01-15 16:35:19 +0000

    added camera function

--- app/src/main/java/com/example/ma18uus/myapplication/Shirts.java
+++ app/src/main/java/com/example/ma18uus/myapplication/Shirts.java
@@ -1,13 +1,28 @@
 package com.example.ma18uus.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Shirts extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shirts);
+        btn=(Button)findViewById(R.id.AddShirts);
+        btn.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivity(i);
+            }
+        });
     }
 }
