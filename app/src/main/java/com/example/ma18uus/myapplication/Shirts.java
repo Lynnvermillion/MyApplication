2c01b739553753ebfb5e237c7d0127ca24d3553e
utpallama <uslama@hotmail.co.uk>
2020-01-15 16:05:43 +0000

    2nd commit

---
+++ app/src/main/java/com/example/ma18uus/myapplication/Shirts.java
@@ -0,0 +1,13 @@
+package com.example.ma18uus.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shirts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shirts);
+    }
+}
